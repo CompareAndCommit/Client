68e93c3aa925d4744b613293bdce9f90543eee5a
SeojinSeojin <1106laura@naver.com>
2021-03-25 03:09:14 +0900

    [Docs] 주석 추가

--- cnc/src/Compare.js
+++ cnc/src/Compare.js
@@ -7,14 +7,6 @@ import axios from "axios"
 import './Compare.css'
 
 function Compare(props) {
-    let [myData, onMyDataChange] = useState({data:[
-        {x: "", y: 0},
-    ]})
-    
-    let [frData, onFrDataChange] = useState({data: [
-        {x: "", y: 0},
-    ]})
-
     //오늘 날짜 구하기
     let today = new Date();
     let year = today.getFullYear();
@@ -40,9 +32,19 @@ function Compare(props) {
         minDate = (year - 1) + '-' + month + '-' + date;
     }
 
+    /*
+    fDate는 끝날짜, tDate는 시작날짜. 
+    위에서 잘 구해놓은 fullDate(오늘)을 fDate로, tmpDate(한달전)을 tDate로 초기화함 
+    구글링 했을 때 많은 분들이 저렇게 객체로 담으셔서 나도 객체로 담았다..
+    fDate.date를 해야 진짜 그 날짜 문자열 값에 접근 가능
+    */
     let [fDate, onFullDateChange] = useState({date:fullDate});
     let [tDate, onTmpDateChange] = useState({date:tmpDate});
 
+    /*
+    switchFullDateHandler(새로운날짜) 라고 함수 호출하면 fdate의 date를 바꾸어준다.
+    switchTmpDateHandler도 마찬가지!
+    */
     const switchFullDateHandler = (newDate) => {
         onFullDateChange({date:newDate})
     }
@@ -51,6 +53,21 @@ function Compare(props) {
         onTmpDateChange({date:newDate})
     }
 
+    /*
+    myData는 내 커밋 데이터
+    frData는 내 친구 커밋 데이터
+    */
+    let [myData, onMyDataChange] = useState({data:[
+        {x: "", y: 0},
+    ]})
+    
+    let [frData, onFrDataChange] = useState({data: [
+        {x: "", y: 0},
+    ]})
+
+    /*
+    위에 있는 switchTmpDateHandler 친구들과 비슷한 로직이다
+    */
     const switchMyDataHandler = (newData) => {
         onMyDataChange({data:newData})
     }
@@ -59,7 +76,20 @@ function Compare(props) {
         onFrDataChange({data:newData})
     }
 
-    //getCommits(props.myName, props.friendName, fullDate, tmpDate)
+    /*
+    useEffect 함수에는 두 개의 인자가 담기는데
+    첫번째 인자는 호출될 함수를
+    두번째 인자는 함수를 호출하는 트리거(?)를 담는다.
+    
+    첫번째 인자로 담긴 함수는 axios로 요청 받아서 열심히 그래프에 데이터를 넣는 부분!!
+    console.log()된 값이나 post맨에서 받은 값으로 어떻게 구성되었는지 볼 수 있다.
+
+    지금 두번째 인자로 [fDate, tDate]가 담겨있는데,
+    위에 state로 정의된
+    fDate가 바뀔 때마다, 혹은 tDate가 바뀔 때마다 위의 함수가 호출된다.
+    아래의 date input하는 부분에서 변화가 생길 때마다 state에 있는 fDate나 tDate를 바꿔줘~ 라고 코드를 짰는데, ( onChange={ev => switchFullDateHandler(ev.target.value)} )
+    그게 실행될 때마다, 즉 사용자가 date를 새로 선택할 때마다 위의 함수가 호출된다.
+    */
     useEffect(()=>{
         axios.get(`/compare_commits?MyName=${props.myName}&OtherName=${props.friendName}&StartDate=${tDate.date}&EndDate=${fDate.date}`).then(
             (response) => {
