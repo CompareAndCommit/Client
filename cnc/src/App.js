a319f1dead5cad6688f80367cd7fc3ae20c95938
Serin <bcode429@gmail.com>
2021-04-02 16:44:56 +0900

    [FIX] this.props -> props

--- cnc/src/App.js
+++ cnc/src/App.js
@@ -1,20 +1,22 @@
 import Footer from "./component/footer"
 import Home from "./Home"
 import Compare from "./Compare"
+import Commit from "./Commit"
 import './App.css';
 import React, {useState} from "react"
 
 function App() {
   const [viewHome, setHome] = useState(true);
   const [viewCompare, setCompare] = useState(false);
-  //const [viewCommit, setCommit] = useState(false);
+  const [viewCommit, setCommit] = useState(false);
   const [myName, setMyName] = useState("");
   const [friendName, setFriendName] = useState("");
 
   return (
     <div className="App">
-      {viewHome? <Home myName={myName} friendName={friendName} setMyName={setMyName} setFriendName={setFriendName} viewHome={viewHome} setHome={setHome} setCompare={setCompare} /> : null}
-      {viewCompare? <Compare myName={myName} friendName={friendName} setHome={setHome} viewCompare={viewCompare} setCompare={setCompare}/> : null}
+      {viewHome? <Home myName={myName} friendName={friendName} setMyName={setMyName} setFriendName={setFriendName} viewHome={viewHome} setHome={setHome} setCompare={setCompare} setCommit={setCommit} /> : null}
+      {viewCompare? <Compare myName={myName} friendName={friendName} setHome={setHome} viewCompare={viewCompare} setCompare={setCompare} setCommit={setCommit}/> : null}
+      {viewCommit? <Commit myName={myName} setHome={setHome} setCompare={setCompare} setCommit={setCommit} viewCommit={viewCommit}/> : null}
       <Footer/>
     </div>
   );
