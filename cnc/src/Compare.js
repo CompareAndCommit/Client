9d96157a136beeb17798e33c196758fbdca6d972
SeojinSeojin <1106laura@naver.com>
2021-04-11 13:52:44 +0900

    [Feat] Arrow 반응형 수정

--- cnc/src/Compare.js
+++ cnc/src/Compare.js
@@ -223,7 +223,7 @@ function Compare(props) {
 
 
                     <div className="wrapper">
-                        <span className="prevArrow">
+                        <div className="arrow-div" id="prevArrow">
                             <svg className="arrowSVG" width="18px" height="17px" viewBox="0 0 18 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
                             onClick={
                                 () => {
@@ -239,11 +239,11 @@ function Compare(props) {
                                     <path d="M-1.48029737e-15,0.56157424 L-1.48029737e-15,16.1929159 L9.708,8.33860465 L-2.66453526e-15,0.56157424 L-1.48029737e-15,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path>
                                 </g>
                             </svg>
-                            <span className="prev">go back to main</span>
-                        </span>
+                            <span>go back to main</span>
+                        </div>
                         
-                        <span className="nextArrow">
-                            <span className="next">go to commit page</span>
+                        <div className="arrow-div" id="nextArrow">
+                            <span>go to commit page</span>
                             <svg className="arrowSVG" width="18px" height="17px" viewBox="-1 0 18 17" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnXlink="http://www.w3.org/1999/xlink"
                             onClick={
                                 () => {
@@ -260,7 +260,7 @@ function Compare(props) {
                                     <path d="M-4.58892184e-16,0.56157424 L-4.58892184e-16,16.1929159 L9.708,8.33860465 L-1.64313008e-15,0.56157424 L-4.58892184e-16,0.56157424 Z M1.33333333,3.30246869 L7.62533333,8.34246869 L1.33333333,13.4327013 L1.33333333,3.30246869 L1.33333333,3.30246869 Z"></path>
                                 </g>
                             </svg>
-                        </span>
+                        </div>
                         
                     </div>
 
