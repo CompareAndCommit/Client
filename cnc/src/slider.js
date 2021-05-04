04174f349ec10becdfa61616a60528067d2d0401
Serin <bcode429@gmail.com>
2021-05-04 22:41:58 +0900

    [FIX] slider.js API 호출

--- cnc/src/slider.js
+++ cnc/src/slider.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import {useEffect, useState } from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
+import axios from 'axios';
+import { myToast } from "./component/swal-toast";
 
 function SimpleSliderElement(props) {
   return (<div className="carousel-element"><img className="carousel-img" src={props.src} alt={props.name}/><div className="carousel-div">{props.name}</div></div>)
@@ -20,6 +23,7 @@ function SampleNextArrow(props) {
 
 function SamplePrevArrow(props) {
   const { className, style, onClick } = props;
+
   return (
     <div
       className={className}
@@ -31,6 +35,7 @@ function SamplePrevArrow(props) {
 
 export default class SimpleSlider extends Component {
   render() {
+
     const settings = {
       dots: true,
       infinite: true,
@@ -58,6 +63,39 @@ export default class SimpleSlider extends Component {
         }
       ]
     };
+
+    const myName = this.props.myName;
+    const friendName = this.props.friendName;
+    
+    async function getUnusedLangData() {
+      axios.get(`/compare-languages?MyName=${myName}&OtherName=${friendName}`).then(
+
+        (response) => {
+          if (response.data.code === 400) {
+            myToast("warning", "Cannot Get Data")
+            setTimeout(()=>{
+              this.props.setCommit(false)
+              this.props.setCompare(true)
+            })
+          }
+          else {
+
+            const langList = response.data.no_commit_lang;
+
+            const elementsData = [
+              [langList[0], "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png"],
+              [langList[1], "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/1024px-Ruby_logo.svg.png"],
+              [langList[2], "https://assets.stickpng.com/images/5847f289cef1014c0b5e486b.png"],
+              [langList[3], "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png"],
+              [langList[4], "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png"]
+            ]
+          }
+        },
+        (error) => {console.log(error)}
+      );
+    }
+    getUnusedLangData();
+
     const elements_data = [
       ["python", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png"],
       ["ruby", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/1024px-Ruby_logo.svg.png"],
@@ -65,7 +103,17 @@ export default class SimpleSlider extends Component {
       ["c++", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png"],
       ["c++", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png"]
     ]
-    const Elements = elements_data.map((el)=>SimpleSliderElement({name:el[0], src:el[1]}))
+
+    const Elements = elements_data.map( (el) => SimpleSliderElement({name:el[0], src:el[1]}) )
+    
+    return (
+      <div>
+        <Slider {...settings}>
+          {Elements}
+        </Slider>
+      </div>
+    );
+
     return (
       <div>
         <Slider {...settings}>
