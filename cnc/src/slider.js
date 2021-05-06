1481b5a0a9b45f74910942728649099a9cba0cf9
Kim Seojin <001106ksj@gmail.com>
2021-05-07 01:36:40 +0900

    [Feat] 슬라이더 퍼블리싱 마무리

--- cnc/src/slider.js
+++ cnc/src/slider.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-import {useEffect, useState } from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import axios from 'axios';
 import { myToast } from "./component/swal-toast";
 
 function SimpleSliderElement(props) {
@@ -23,7 +21,6 @@ function SampleNextArrow(props) {
 
 function SamplePrevArrow(props) {
   const { className, style, onClick } = props;
-
   return (
     <div
       className={className}
@@ -34,6 +31,37 @@ function SamplePrevArrow(props) {
 }
 
 export default class SimpleSlider extends Component {
+  state = {}
+
+  async callAPI() {
+    const myName = this.props.myName;
+    const friendName = this.props.friendName;
+    return fetch(`/compare-languages?MyName=${myName}&OtherName=${friendName}`)
+    .then(response => response.json())
+    .then(json => json.no_commit_lang)
+    .catch(err => {
+      console.log(err)
+      myToast("warning", "Cannot Get Data")
+      setTimeout(()=>{
+        this.props.setCommit(false)
+        this.props.setCompare(true)
+      })
+    })
+  }
+
+  async getUnusedLangData(){
+    const datas = await this.callAPI();
+    this.setState(datas)
+  }
+
+  componentDidMount() {
+    this.getUnusedLangData()
+  }
+
+  renderLanguages() {
+    return Object.values(this.state).map((el) => SimpleSliderElement({name:el, src:"https://upload.wikimedia.org/wikipedia/en/thumb/4/40/Sungkyunkwan_University_seal.svg/1200px-Sungkyunkwan_University_seal.svg.png"}))
+  }
+
   render() {
 
     const settings = {
@@ -42,84 +70,23 @@ export default class SimpleSlider extends Component {
       speed: 500,
       nextArrow: <SampleNextArrow />,
       prevArrow: <SamplePrevArrow />,
-      slidesToShow:3,
+      slidesToShow: 2,
       slidesToScroll: 1,
       initialSlide: 0,
-      responsive: [
-        {
-          breakpoint: 700,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
-          }
-        },
-        {
-          breakpoint: 550,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
+      autoplay: true,
+      autoplaySpeed: 1500,
+      pauseOnHover: true
     };
 
-    const myName = this.props.myName;
-    const friendName = this.props.friendName;
-    
-    async function getUnusedLangData() {
-      axios.get(`/compare-languages?MyName=${myName}&OtherName=${friendName}`).then(
-
-        (response) => {
-          if (response.data.code === 400) {
-            myToast("warning", "Cannot Get Data")
-            setTimeout(()=>{
-              this.props.setCommit(false)
-              this.props.setCompare(true)
-            })
-          }
-          else {
-
-            const langList = response.data.no_commit_lang;
-
-            const elementsData = [
-              [langList[0], "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png"],
-              [langList[1], "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/1024px-Ruby_logo.svg.png"],
-              [langList[2], "https://assets.stickpng.com/images/5847f289cef1014c0b5e486b.png"],
-              [langList[3], "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png"],
-              [langList[4], "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png"]
-            ]
-          }
-        },
-        (error) => {console.log(error)}
-      );
-    }
-    getUnusedLangData();
-
-    const elements_data = [
-      ["python", "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/768px-Python-logo-notext.svg.png"],
-      ["ruby", "https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ruby_logo.svg/1024px-Ruby_logo.svg.png"],
-      ["dart", "https://assets.stickpng.com/images/5847f289cef1014c0b5e486b.png"],
-      ["c++", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png"],
-      ["c++", "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1200px-ISO_C%2B%2B_Logo.svg.png"]
-    ]
-
-    const Elements = elements_data.map( (el) => SimpleSliderElement({name:el[0], src:el[1]}) )
-    
-    return (
-      <div>
-        <Slider {...settings}>
-          {Elements}
-        </Slider>
-      </div>
-    );
+    const elements = this.state
 
     return (
       <div>
         <Slider {...settings}>
-          {Elements}
+          {elements ? this.renderLanguages() : "loading"}
         </Slider>
       </div>
     );
+
   }
 }
